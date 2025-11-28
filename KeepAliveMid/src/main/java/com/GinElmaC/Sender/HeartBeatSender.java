a74fe04951679f5b1fa96a91913ded973a4ba526
wangyishuo <2227172350@qq.com>
2025-11-28 18:25:58 +0800

    2025.11.28

---
+++ KeepAliveMid/src/main/java/com/GinElmaC/Sender/HeartBeatSender.java
@@ -0,0 +1,16 @@
+package com.GinElmaC.Sender;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 心跳Beat发送
+ */
+@Component
+public class HeartBeatSender {
+    @PostConstruct
+    public void init(){
+
+    }
+}
--- MessagePushMidCommend/src/main/java/com/GinElmaC/entity/Message/HeartBeatMessage.java
+++ MessagePushMidCommend/src/main/java/com/GinElmaC/entity/Message/HeartBeatMessage.java
@@ -4,12 +4,7 @@ package com.GinElmaC.entity.Message;
  * 服务器之间传输心跳包
  */
 public class HeartBeatMessage {
-    //消息的类型
-    String MsgType = "heartbeat";
-    //标识信息，用来标识发送服务器以及目标服务器
-    //时间戳，用来标识发送的时间戳
-    //序列号
-    //目标序列号，当接收的服务器
-    //负载信息
-    //检查字段
+    //基础消息类型
+    BaseMessage baseMessage;
+
 }
